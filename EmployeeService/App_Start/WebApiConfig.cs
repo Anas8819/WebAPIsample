03afda329709e612f13900fc6521a7689f0c5883
Anas8819 <anasahmed8819@hotmail.com>
2017-09-24 14:55:39 +0500

    Checking

--- EmployeeService/App_Start/WebApiConfig.cs
+++ EmployeeService/App_Start/WebApiConfig.cs
@@ -12,7 +12,6 @@ using System.Web.Http.Cors;
 
 namespace EmployeeService
 {
-    //fcd
     //public class CustomJsonFormeter : JsonMediaTypeFormatter
     //{
     //    public CustomJsonFormeter()
